332b66517045a0adef4dc394ddd32315138221c7
Vivek Aher <69898929+vivek4879@users.noreply.github.com>
2023-06-19 17:23:15 +0530

    Time: 125 ms (27.27%), Space: 22 MB (5.67%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,23 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        mydict={}
-        res = []
-        res1=[]
-        for i in nums:
-            if i not in mydict:
-                mydict[i] = 1
-            else:
-                mydict[i] = mydict[i] + 1
-        sorted_dict = sorted(mydict.items(), key=lambda x: x[1], reverse = True)
-        for i,j in sorted_dict:
-            res.append (i)
-
-        for i in range(k):
-            res1.append(res[i])
-        return res1
+        count_dict = {} 
+        freq = [[] for i in range(len(nums) +1)]
+        
+        # creating a dictionary which has counts of each element of nums
+        for i in nums: 
+            count_dict[i] = 1 + count_dict.get(i, 0)
+        
+        #creating a list of lists with each list consisting elements which have  frequency equal to the index of that list.
+        for i,j in count_dict.items(): 
+            freq[j].append(i)
+            
+        #creating result array
+        res=[]
+        
+        #here we go through the freq aray in reverse order hence starting from end of array and going till zero as we want to return the most frequent elements.
+        for i in range(len(freq) - 1, 0 , -1):
+            for j in freq[i]:
+                res.append(j)
+                if len(res) == k:
+                    return res
+        
