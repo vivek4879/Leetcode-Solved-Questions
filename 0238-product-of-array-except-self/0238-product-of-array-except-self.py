9d42d4b69f71661773146500c56f7f543986ef18
Vivek Aher <69898929+vivek4879@users.noreply.github.com>
2023-06-22 02:04:11 +0530

    Time: 234 ms (77.02%), Space: 23.5 MB (82.80%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,10 +3,15 @@ class Solution:
         ps = 1
         pss = 1
         res=[1]
-
+        
+        #calculating prefix(product of all elements before the one in consideration) and storing in the result array
+        
         for i in range(len(nums)-1):
             ps=ps*nums[i]
             res.append(ps)
+        
+        #calculating postfix(product of all elements after the one in consideration) and then multiplying it with the prefixes already stored in res arrray. This way we only need one res array.
+        
         
         for i in range(-1, -(len(nums)+1), -1):
             res[i]= res[i] * pss
