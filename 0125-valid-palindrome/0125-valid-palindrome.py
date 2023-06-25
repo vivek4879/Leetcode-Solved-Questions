72273cf4fcfbfc2ed9cd5be1a9702796bc7a5fe2
Vivek Aher <69898929+vivek4879@users.noreply.github.com>
2023-06-25 18:05:32 +0530

    Time: 103 ms (5.26%), Space: 16.8 MB (96.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,26 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        #making an emprty string
-        x = ''
+    #initiating two pointers one from start and one from the end
+        m , n = 0 , len(s) - 1
+    
+    #using a while loop to ensure that the pointers don't cross each other
+        while m < n:
+    # if the element in consideration is not alphanumeric as checked by our function, we increment its pointer to check for the next element.
+            while m < n and not self.alphanum(s[m]):
+                m+=1
+            while n > m and not self.alphanum(s[n]):
+                n-=1      
+    # once we have established that the element in consideration is alphanumeric, we check if it is lower and compare the element in the start and in the end. We return false if they are not same as then it won't be a palindrome. We increment the starting counter and decrease the ending counter to check next if not false.
+            if s[m].lower() != s[n].lower():
+                return False
+            m , n = m+1, n-1
         
-        #going through every character in the given string and checking if it is alphanumeric
-        for i in s:
-            if i.isalnum():
-                x += i.lower()
+        return True
                 
-        #checking if x and reverse of x are equal
-        return (x == x[::-1])
+        
+        
+    #defining a function to check if the element in consideration is alphanumeric or not
+    def alphanum(self, i):
+        return(ord('A') <= ord(i) <= ord('Z') or
+        ord('a') <= ord(i) <= ord('z') or
+        ord('0') <= ord(i) <= ord('9'))
