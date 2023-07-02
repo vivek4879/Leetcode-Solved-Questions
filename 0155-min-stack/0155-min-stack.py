e16fa30cfd9ea3425f6a5382b5848ba680c79939
Vivek Aher <69898929+vivek4879@users.noreply.github.com>
2023-07-02 23:06:47 +0530

    Time: 66 ms (91.43%), Space: 20.5 MB (48.68%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,17 +1,20 @@
 class MinStack:
 
     def __init__(self):
+        #we initiate 2 stacks, one to store the pushed values so that we can get the top element when needed, and other to store the minimum element at each iteration.
         self.stack = []
         self.minstack = []
         
 
     def push(self, val: int) -> None:
+        #we use append function to push the values in our stack
         self.stack.append(val)
+        #to push values in our minstack, we first check if the current value pushed is less than the element already at the top of the stack and append whichever is less at that point
         if self.minstack:
             val= min(val,self.minstack[-1])
         self.minstack.append(val)
             
-            
+        #we use pop function to remove the top element in both our stacks.
     def pop(self) -> None:
         self.stack.pop()
         self.minstack.pop()
