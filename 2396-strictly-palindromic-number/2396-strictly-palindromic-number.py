43403aaaacde0f0062b42214b29c201b936e28ec
Vivek Aher <69898929+vivek4879@users.noreply.github.com>
2023-07-02 12:03:27 +0530

    Time: 53 ms (19.75%), Space: 16.4 MB (27.62%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,13 +1,18 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
+        #we iterate starting from 2 and uptop n-1 to include n-2
         for i in range(2, n-1):
+        #we store the value of n in another variable m so that we can work on m  
+        #we also initiate a string res
             m = n
-            res=[]
+            res=''
+        #to convert to the respective base we use a while look until the m is no longer divisible by i
             while m >= i:
-                res.append(str(m % i))
+                res= res + (str(m % i))
                 m = m//i
-            res.append(str(m))
-            if ''.join(res) != ''.join(res[::-1]):
+            res = res + str(m)
+        #we use string manipulation to check if they are palindrome and return false if they are not
+            if res != res[::-1]:
                 return False
         return True
             
