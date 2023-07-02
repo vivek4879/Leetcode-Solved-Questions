15c99e4a46fde6aa2248c9f443e64b5bc35d9e03
Vivek Aher <69898929+vivek4879@users.noreply.github.com>
2023-07-02 15:06:03 +0530

    Time: 50 ms (40.13%), Space: 16.2 MB (90.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,9 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        #we create a stack which will store opening parentheses if we encounter one in our loop
         stack = []
+        #we create a hashmap in whihc we store all the closing parentheses as keys
         parentdict = {')' : '(', ']': '[', '}': '{'}
         
+        #iterating through the input string s
         for i in s:
+        #we check if i is a clsoing parenthesis by checking if its in out dict or not. if it is not in our dict, its an opening parenthesis and we then add it to our stack. If its a closing parenthesis then we further check if its opening counterpart is at the top of our stack, if it is, we just remove it from top of stack and carry on, if it is not then we return false. 
             if i in parentdict:
                 if stack and parentdict[i] == stack[-1]:
                     stack.pop()
@@ -11,7 +15,7 @@ class Solution:
                     return False
             else:
                 stack.append(i)
-                
+       #we return true is our stack is empty in the end as that means all opening parentheses has closings too.         
         return True if not stack else False
                 
             
