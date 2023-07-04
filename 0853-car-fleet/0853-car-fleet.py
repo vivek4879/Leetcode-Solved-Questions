b13f81895fee7cb6cdf4b487624eb9641d76d512
Vivek Aher <69898929+vivek4879@users.noreply.github.com>
2023-07-04 13:22:36 +0530

    Time: 1007 ms (36.40%), Space: 36.3 MB (76.36%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,14 +1,9 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        stack = [] #stack to track speeds
-        new = [[p,s] for p,s in zip(position,speed)] #this array will hold all the position and speed pairs of each car. This array is made using python list composition
-        
-        for p , s in sorted(new)[::-1]: #looping through the sorted reversed list
-            time = (target - p) / s     #finding time for each car to reach the target from its current position
-            if stack and stack[-1] < time:   # if stack not empty and the time at top of stack is less than time for current car then we append new time
-                stack.append(time)
-            elif stack and stack[-1] >= time:   #if stack not empty and speed at top of stackgreater than or equal to the time for current car then we continue to next car
-                continue
-            else:                      #if stack empty then we append the speed to the stack
-                stack.append(time)
+        stack = [] 
+        new = [[p,s] for p,s in zip(position,speed)] 
+        for p , s in sorted(new)[::-1]: 
+            stack.append((target - p) / s)     
+            if len(stack)>=2 and stack[-1] <= stack[-2]:
+                stack.pop()
         return len(stack)
